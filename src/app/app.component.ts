ecabbac1530d688c73d46eb03e640278a769d2fa
udayBettala <udayb463@gmail.com>
2020-02-15 19:28:22 -0500

    Adding buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  d : number = 40;
+
+  ngOnInit() {
+  }
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SaladFormComponent } from './components/salad-form/salad-form.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const appRoutes: Routes = [
+  { path: 'buildSalad', component: SaladFormComponent}
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +16,10 @@ import { SaladFormComponent } from './components/salad-form/salad-form.component
     SaladFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule,RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
